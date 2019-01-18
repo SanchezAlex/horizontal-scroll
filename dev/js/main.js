9c56827ea8318345896c313b59630b015fbb38a1
SanchezAlex <malinovskiy.alexandr@gmail.com>
2019-01-18 04:10:34 +0200

    add eventlistener for touches

--- dev/js/main.js
+++ dev/js/main.js
@@ -45,6 +45,61 @@ class App {
 			document.body.classList.remove('no-scroll');
 		}, 1000);
 
+
+		// detect swipe
+
+		function detectSwipe() {
+			// Swipe Up / Down / Left / Right
+			var initialX = null;
+			var initialY = null;
+
+			function startTouch(e) {
+				initialX = e.touches[0].clientX;
+				initialY = e.touches[0].clientY;
+			}
+
+			function moveTouch(e) {
+				if (initialX === null) {
+					return;
+				}
+
+				if (initialY === null) {
+					return;
+				}
+
+				var currentX = e.touches[0].clientX;
+				var currentY = e.touches[0].clientY;
+
+				var diffX = initialX - currentX;
+				var diffY = initialY - currentY;
+
+				if (Math.abs(diffX) > Math.abs(diffY)) {
+					// sliding horizontally
+					if (diffX > 0) {
+						// swiped left
+						console.log("swiped left");
+					} else {
+						// swiped right
+						console.log("swiped right");
+					}
+				} else {
+					// sliding vertically
+					if (diffY > 0) {
+						// swiped up
+						console.log("swiped up");
+					} else {
+						// swiped down
+						console.log("swiped down");
+					}
+				}
+
+				initialX = null;
+				initialY = null;
+
+				e.preventDefault();
+			}
+		}
+		detectSwipe();
 	}
 
 
@@ -99,6 +154,10 @@ class App {
 
 		document.addEventListener("mousewheel", initSlide);
 		document.addEventListener("DOMMouseScroll", initSlide);
+
+		document.addEventListener("touchstart", initSlide);
+		document.addEventListener("touchmove", initSlide);
+
 	}
 
 	run() {
